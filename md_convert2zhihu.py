5791dc64037d7fc5467e1b707fb946b0ad7049aa
tonywenuon <tonywen.reg@hotmail.com>
2019-10-18 00:16:21 +0100

    fix bug in conver2zhihu

--- md_convert2zhihu.py
+++ md_convert2zhihu.py
@@ -1,7 +1,7 @@
 import re
 def repl(m):
     inner_word = m.group(1)
-    return '<br><br>$' + inner_word + '$<br><br>'
+    return '<br><br>$' + inner_word.strip() + '$<br><br>'
 with open('1', 'r') as f_read:
     text = f_read.readlines()
     for k, item in enumerate(text):
