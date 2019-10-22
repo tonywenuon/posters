a9f5c01b8c7a2bc14077a7c363e4495c0ba6eeea
tonywenuon <tonywen.reg@hotmail.com>
2019-10-22 12:50:51 +0100

    update convert tool

--- md_convert2zhihu.py
+++ md_convert2zhihu.py
@@ -1,5 +1,5 @@
 import re
-def repl(m):
+def repl1(m):
     inner_word = m.group(1) 
     #return '<br><br>$' + inner_word.strip() + '$<br><br>'
     remove_space = ''
@@ -8,11 +8,20 @@ def repl(m):
             remove_space += ch
     return '<br><br>![](https://latex.codecogs.com/gif.latex?' + remove_space.strip() + ')<br><br>'
 
+def repl2(m):
+    inner_word = m.group(1) 
+    #return '<br><br>$' + inner_word.strip() + '$<br><br>'
+    remove_space = ''
+    for ch in inner_word:
+        if ch != ' ':
+            remove_space += ch
+    return '![](https://latex.codecogs.com/gif.latex?' + remove_space.strip() + ')'
+
 with open('Important1_seq2seq_attention.md', 'r') as f_read:
     text = f_read.readlines()
     for k, item in enumerate(text):
-        after_rep = re.sub(r'\$\$(.*?)\$\$', repl, item)
-        after_rep = re.sub(r'\$(.*?)\$', repl, after_rep)
+        after_rep = re.sub(r'\$\$(.*?)\$\$', repl1, item)
+        after_rep = re.sub(r'\$(.*?)\$', repl2, after_rep)
         text[k] = after_rep
     
     with open('2.md', 'w') as f_write:
