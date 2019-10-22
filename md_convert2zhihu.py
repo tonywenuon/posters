b4df6d11d9e9538ba32f727e9f4d4efab42995e1
tonywenuon <tonywen.reg@hotmail.com>
2019-10-22 12:45:39 +0100

    update convert tool

--- md_convert2zhihu.py
+++ md_convert2zhihu.py
@@ -1,11 +1,19 @@
 import re
 def repl(m):
     inner_word = m.group(1) 
-    return '<br><br>$' + inner_word.strip() + '$<br><br>'
-with open('1', 'r') as f_read:
+    #return '<br><br>$' + inner_word.strip() + '$<br><br>'
+    remove_space = ''
+    for ch in inner_word:
+        if ch != ' ':
+            remove_space += ch
+    return '![](https://latex.codecogs.com/gif.latex?' + remove_space.strip() + ')'
+
+with open('Important1_seq2seq_attention.md', 'r') as f_read:
     text = f_read.readlines()
     for k, item in enumerate(text):
-        text[k] = re.sub(r'\$\$(.*?)\$\$', repl, item)
+        after_rep = re.sub(r'\$\$(.*?)\$\$', repl, item)
+        after_rep = re.sub(r'\$(.*?)\$', repl, after_rep)
+        text[k] = after_rep
     
-    with open('2', 'w') as f_write:
+    with open('2.md', 'w') as f_write:
         f_write.writelines(text)
